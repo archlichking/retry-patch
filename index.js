036668eca697b2b0d1c88bd6f0ea72783b202b33
Lei Zhu <lzhu@walmart.com>
2014-10-22 16:01:22 -0700

    cwd enabled

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // for running
 var Patch = require('./retryPatch');
+var path = require('path');
 
 if (process.argv.length < 4) {
     console.log('usage: \'node retryPatch.js MOCHA_JSON_REPORT_FILE MOCHA_TEST_FILE \'');
     process.exit(1);
 }
 
-var MOCHA_TEST_FILE = process.argv[3];
-var MOCHA_JSON_REPORT_FILE = process.argv[2];
+var MOCHA_TEST_FILE = path.resolve(process.cwd(), process.argv[3]);
+var MOCHA_JSON_REPORT_FILE = path.resolve(process.cwd(), process.argv[2]);
 
 var p = new Patch(MOCHA_JSON_REPORT_FILE, MOCHA_TEST_FILE);
 
